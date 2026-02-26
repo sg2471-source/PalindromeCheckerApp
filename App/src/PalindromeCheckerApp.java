d34b81f3e76403da366d0d8eb6862a44af829ced
sg2471-source <sg2471@srmist.edu.in>
2026-02-26 15:30:05 +0530

    UC1 WELCOME PAGE

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,11 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        //USECASE-1
+        System.out.println("Welcome To Palindrome Checker APP Management SYSTEM");
+    }
+
+}
+
+
+//USECASE-2
+
