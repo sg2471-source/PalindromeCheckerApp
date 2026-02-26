ab8ea9a32c25dc492958368cf0dbfb051797d26d
sg2471-source <sg2471@srmist.edu.in>
2026-02-26 15:54:10 +0530

    Shreyaas ADDED USECASE-2

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,16 @@
+public class PalindromeCheckerApp {
+    static void main() {
+        String word = "madam";
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed += word.charAt(i);
+        }
+
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a palindrome");
+        } else {
+            System.out.println(word + " is not a palindrome");
+        }
+    }
+}
