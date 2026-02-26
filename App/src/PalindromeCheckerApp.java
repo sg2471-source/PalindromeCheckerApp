c5cf1cde3c6e8be9e1692532bd107c30f5774d77
sg2471-source <sg2471@srmist.edu.in>
2026-02-26 16:28:12 +0530

    SHREYAAS ADDED USECASE -6

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -6,23 +8,32 @@ public class PalindromeCheckerApp {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a string:");
         String input = sc.nextLine();
-        String processed = input.replaceAll("\\s+", "").toLowerCase();
+        String processed = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
+
         for (int i = 0; i < processed.length(); i++) {
-            stack.push(processed.charAt(i));
+            char ch = processed.charAt(i);
+            queue.add(ch);
+            stack.push(ch);
         }
+
         boolean isPalindrome = true;
-        for (int i = 0; i < processed.length(); i++) {
-            if (processed.charAt(i) != stack.pop()) {
+
+        while (!queue.isEmpty()) {
+            if (!queue.remove().equals(stack.pop())) {
                 isPalindrome = false;
                 break;
             }
         }
+
         if (isPalindrome) {
             System.out.println("The given string is a Palindrome");
         } else {
             System.out.println("The given string is not a Palindrome");
         }
+
         sc.close();
     }
 }
